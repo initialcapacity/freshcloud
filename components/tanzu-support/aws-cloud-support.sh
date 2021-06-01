a6a42936bc9c4636793c0c9e6159cff998e59fc3
Mark Alston <malston@vmware.com>
2021-06-01 21:53:10 +0000

    Add support for creating mgmt cluster

--- components/tanzu-support/aws-cloud-support.sh
+++ components/tanzu-support/aws-cloud-support.sh
@@ -6,12 +6,24 @@
 source ../../.env_development.sh
 
 function tanzu_aws_create_k8s_mgmt_cluster() {
-  echo "Not yet implemented"
+  cat > ../../mgmt-cluster.yaml <<EOF
+CLUSTER_NAME: tanzu-mgmt
+CLUSTER_PLAN: dev
+INFRASTRUCTURE_PROVIDER: aws
+ENABLE_CEIP_PARTICIPATION: false
+ENABLE_AUDIT_LOGGING: false
+CONTROL_PLANE_MACHINE_TYPE: t3.large
+NODE_MACHINE_TYPE: m5.large
+AWS_REGION: us-east-2
+AWS_NODE_AZ: "us-east-2a"
+AWS_SSH_KEY_NAME: default
+EOF
+  tanzu management-cluster create --file ../../mgmt-cluster.yaml
 }
 
 function tanzu_aws_create_k8s_cluster() {
 
-  cat > ../../freshcloud-cluster.yaml << EOF
+  cat > ../../freshcloud-cluster.yaml <<EOF
 CLUSTER_NAME: freshcloud
 CLUSTER_PLAN: dev
 NAMESPACE: default
@@ -22,8 +34,8 @@ CONTROL_PLANE_MACHINE_COUNT: 1
 WORKER_MACHINE_COUNT: 3
 AWS_REGION: us-east-2
 AWS_NODE_AZ: "us-east-2a"
-AWS_ACCESS_KEY_ID: <encoded:$(echo -ne $AWS_ACCESS_KEY_ID | base64)>
-AWS_SECRET_ACCESS_KEY: <encoded:$(echo -ne $AWS_SECRET_ACCESS_KEY | base64)>
+AWS_ACCESS_KEY_ID: <encoded:$(echo -ne "$AWS_ACCESS_KEY_ID" | base64)>
+AWS_SECRET_ACCESS_KEY: <encoded:$(echo -ne "$AWS_SECRET_ACCESS_KEY" | base64)>
 AWS_SSH_KEY_NAME: default
 BASTION_HOST_ENABLED: true
 EOF
@@ -38,6 +50,11 @@ if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
   printf "Please set the following environment variables:\n"
   printf "\tAWS_ACCESS_KEY_ID\n"
   printf "\tAWS_SECRET_ACCESS_KEY\n"
+fi
+
+if [ "$1" == 'mgmt' ]; then
+  tanzu_aws_create_k8s_mgmt_cluster
+  exit 0
 fi
 
 if [ "$1" == 'delete' ]; then
