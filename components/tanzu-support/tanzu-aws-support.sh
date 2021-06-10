874f004b81831dde9f21329f4651cc9b7a293656
Mark Alston <malston@vmware.com>
2021-06-10 15:09:55 +0000

    Add extra arg for cluster name

--- components/tanzu-support/tanzu-aws-support.sh
+++ components/tanzu-support/tanzu-aws-support.sh
@@ -8,8 +8,9 @@ source ../../.env_development.sh
 
 function tanzu_aws_create_k8s_mgmt_cluster() {
   local temp_dir=${1:-/tmp}
+  local cluster_name=${2:-tanzu-mgmt}
   cat > "$temp_dir/mgmt-cluster.yaml" <<EOF
-CLUSTER_NAME: tanzu-mgmt
+CLUSTER_NAME: $cluster_name
 CLUSTER_PLAN: dev
 INFRASTRUCTURE_PROVIDER: aws
 ENABLE_CEIP_PARTICIPATION: false
@@ -21,11 +22,10 @@ AWS_NODE_AZ: "us-east-2a"
 AWS_ACCESS_KEY_ID: <encoded:$(echo -ne "$AWS_ACCESS_KEY_ID" | base64)>
 AWS_SECRET_ACCESS_KEY: <encoded:$(echo -ne "$AWS_SECRET_ACCESS_KEY" | base64)>
 AWS_SSH_KEY_NAME: default
-BASTION_HOST_ENABLED: true
+BASTION_HOST_ENABLED: false
 AWS_PRIVATE_NODE_CIDR: 10.0.0.0/24
 AWS_PUBLIC_NODE_CIDR: 10.0.1.0/24
 AWS_VPC_CIDR: 10.0.0.0/16
-AWS_SSH_KEY_NAME: default
 SERVICE_CIDR: 100.64.0.0/13
 CLUSTER_CIDR: 100.96.0.0/11
 EOF
@@ -34,13 +34,15 @@ EOF
 }
 
 function tanzu_aws_delete_k8s_mgmt_cluster() {
-  tanzu management-cluster delete tanzu-mgmt
+  local cluster_name=${1:-tanzu-mgmt}
+  tanzu management-cluster delete "$cluster_name"
 }
 
 function tanzu_aws_create_k8s_cluster() {
   local temp_dir=${1:-/tmp}
+  local cluster_name=${2:-freshcloud}
   cat > "$temp_dir/cluster.yaml" <<EOF
-CLUSTER_NAME: freshcloud
+CLUSTER_NAME: $cluster_name
 CLUSTER_PLAN: dev
 NAMESPACE: default
 CNI: antrea
@@ -53,14 +55,15 @@ AWS_NODE_AZ: $NODE_AZ
 AWS_ACCESS_KEY_ID: <encoded:$(echo -ne "$AWS_ACCESS_KEY_ID" | base64)>
 AWS_SECRET_ACCESS_KEY: <encoded:$(echo -ne "$AWS_SECRET_ACCESS_KEY" | base64)>
 AWS_SSH_KEY_NAME: default
-BASTION_HOST_ENABLED: true
+BASTION_HOST_ENABLED: false
 EOF
   tanzu cluster create -f "$temp_dir/cluster.yaml"
   printf "Workload cluster file saved to: %s\n" "$temp_dir/cluster.yaml"
 }
 
 function tanzu_aws_delete_k8s_cluster() {
-  tanzu cluster delete freshcloud
+  local cluster_name=${1:-freshcloud}
+  tanzu cluster delete "$cluster_name"
 }
 
 if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
@@ -74,15 +77,15 @@ temp_dir=$(mktemp -d -t cluster-XXXXXXXXXX)
 
 if [ "$1" == 'mgmt' ]; then
   if [ "$2" == 'delete' ]; then
-    tanzu_aws_delete_k8s_mgmt_cluster
+    tanzu_aws_delete_k8s_mgmt_cluster "$3"
   else
-    tanzu_aws_create_k8s_mgmt_cluster "$temp_dir"
+    tanzu_aws_create_k8s_mgmt_cluster "$temp_dir" "$2"
   fi
   exit 0
 fi
 
 if [ "$1" == 'delete' ]; then
-  tanzu_aws_delete_k8s_cluster
+    tanzu_aws_delete_k8s_cluster "$2"
 else
-  tanzu_aws_create_k8s_cluster "$temp_dir"
+  tanzu_aws_create_k8s_cluster "$temp_dir" "$2"
 fi
