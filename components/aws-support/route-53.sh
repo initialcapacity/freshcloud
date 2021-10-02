26ef9c247c8933dda2ac5d6f558a3d97da929199
Joey McDonald <jmcdice@gmail.com>
2021-10-02 14:24:47 -0600

    Add support for route 53 in env config.

--- components/aws-support/route-53.sh
+++ components/aws-support/route-53.sh
@@ -4,13 +4,17 @@
 
 source ../../.env_development.sh
 
-ZONE_ID=''
+if [ -z ${ZONE_ID} ]; then
+  echo "Please set ZONE_ID. in .env_development.sh"
+  exit 127
+fi
+
 LB=$(kubectl describe svc ingress-contour-envoy --namespace projectcontour | grep Ingress | awk '{print $3}')
 
 echo "Setting: *.${DOMAIN} to ${LB}"
 aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} \
-  --change-batch '{ "Comment": "CREATE/DELETE/UPSERT a record ", "Changes": [{ "Action": "UPSERT", "ResourceRecordSet": { "Name": "'*.${DOMAIN}'", "Type": "A", "TTL": 300, "ResourceRecords": [{ "Value": "'${LB}'"}] }}] }' &> /dev/null
-sleep 5
+  --change-batch '{ "Comment": "CREATE/DELETE/UPSERT a record ", "Changes": [{ "Action": "UPSERT", "ResourceRecordSet": { "Name": "'*.${DOMAIN}'", "Type": "A", "TTL": 300, "ResourceRecords": [{ "Value": "'${LB}'"}] }}] }' 
+sleep 30
 
 # -- Test
 VAR=$(host app.${DOMAIN} | awk '{print $NF}')
