650293e6ed79946d6404d55522e19dcfee935c59
barinek <barinek@initialcapacity.io>
2022-05-11 23:14:44 -0600

    github resources support
    Signed-off-by: barinek <barinek@initialcapacity.io>

--- pkg/templatesupport/template_support_test.go
+++ pkg/templatesupport/template_support_test.go
@@ -1,10 +1,14 @@
 package templatesupport_test
 
 import (
+	"errors"
 	"github.com/initialcapacity/freshcloud/pkg/templatesupport"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
 	"path/filepath"
 	"runtime"
+	"strings"
 	"testing"
 )
 
@@ -13,6 +17,36 @@ func TestParse(t *testing.T) {
 		Name string
 	}{Name: "world"})
 	assert.Equal(t, "hi world", parsed)
+}
+
+func TestParseViaUrl(t *testing.T) {
+	templatesupport.Client = &http.Client{}
+	location := "https://raw.githubusercontent.com/initialcapacity/freshcloud/main/pkg/templatesupport/resources"
+	parsed := templatesupport.Parse(location, "test", struct {
+		Name string
+	}{Name: "world"})
+	assert.Equal(t, "hi world", parsed)
+}
+
+type mock struct {
+	err error
+}
+
+func (m mock) Get(url string) (resp *http.Response, err error) {
+	return &http.Response{StatusCode: 200, Body: ioutil.NopCloser(strings.NewReader(""))}, m.err
+}
+
+func TestParseViaUrl_GetFails(t *testing.T) {
+	templatesupport.Client = mock{err: errors.New("oops")}
+	defer func() {
+		if recover() == nil {
+			t.Fail()
+		}
+	}()
+	location := "https://raw.githubusercontent.com/"
+	_ = templatesupport.Parse(location, "test", struct {
+		Name string
+	}{Name: "world"})
 }
 
 func TestParse_badTemplate(t *testing.T) {
