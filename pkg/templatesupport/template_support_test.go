0ee97fad0a975dee0702b08819500e0cafa7c7ec
barinek <barinek@initialcapacity.io>
2022-06-22 07:54:12 -0600

    using a template for displaying version number
    Signed-off-by: barinek <barinek@initialcapacity.io>

--- pkg/templatesupport/template_support_test.go
+++ pkg/templatesupport/template_support_test.go
@@ -13,27 +13,28 @@ import (
 )
 
 func TestParse(t *testing.T) {
-	parsed := templatesupport.Parse("", "test", struct {
-		Name string
-	}{Name: "world"})
-	assert.Equal(t, "hi world", parsed)
+	parsed := templatesupport.Parse("", "version", struct {
+		Version string
+	}{Version: "1.0"})
+	assert.Equal(t, "freshctl version 1.0", parsed)
 }
 
 func TestParseViaUrl(t *testing.T) {
-	templatesupport.Client = &http.Client{}
+	templatesupport.Client = mock{body: "freshctl version 2.0"}
 	location := "https://raw.githubusercontent.com/initialcapacity/freshcloud/main/pkg/freshctl/resources"
-	parsed := templatesupport.Parse(location, "test", struct {
-		Name string
-	}{Name: "world"})
-	assert.Equal(t, "hi world", parsed)
+	parsed := templatesupport.Parse(location, "version", struct {
+		Version string
+	}{Version: "2.0"})
+	assert.Equal(t, "freshctl version 2.0", parsed)
 }
 
 type mock struct {
-	err error
+	err  error
+	body string
 }
 
 func (m mock) Get(url string) (resp *http.Response, err error) {
-	return &http.Response{StatusCode: 200, Body: ioutil.NopCloser(strings.NewReader(""))}, m.err
+	return &http.Response{StatusCode: 200, Body: ioutil.NopCloser(strings.NewReader(m.body))}, m.err
 }
 
 func TestParseViaUrl_GetFails(t *testing.T) {
@@ -44,19 +45,19 @@ func TestParseViaUrl_GetFails(t *testing.T) {
 		}
 	}()
 	location := "https://raw.githubusercontent.com/"
-	_ = templatesupport.Parse(location, "test", struct {
-		Name string
-	}{Name: "world"})
+	_ = templatesupport.Parse(location, "version", struct {
+		Version string
+	}{Version: "3.4"})
 }
 
 func TestParseViaLocation(t *testing.T) {
 	tempDir := os.TempDir()
-	_ = ioutil.WriteFile(filepath.Join(tempDir, "test.sh"), []byte("hi {{.Name}}"), 0644)
+	_ = ioutil.WriteFile(filepath.Join(tempDir, "version.sh"), []byte("freshctl version {{index .Version}}"), 0644)
 
-	resp := templatesupport.Parse(tempDir, "test", struct {
-		Name string
-	}{Name: "world"})
-	assert.Equal(t, "hi world", string(resp))
+	resp := templatesupport.Parse(tempDir, "version", struct {
+		Version string
+	}{Version: "4.2"})
+	assert.Equal(t, "freshctl version 4.2", string(resp))
 }
 
 func TestParseViaLocation_missingResources(t *testing.T) {
@@ -67,11 +68,11 @@ func TestParseViaLocation_missingResources(t *testing.T) {
 	}()
 
 	tempDir := os.TempDir()
-	_ = ioutil.WriteFile(filepath.Join(tempDir, "test.sh"), []byte("hi {{.Name}}"), 0644)
+	_ = ioutil.WriteFile(filepath.Join(tempDir, "version.sh"), []byte("freshctl version {{index .Version}}"), 0644)
 
-	_ = templatesupport.Parse(tempDir, "tes", struct {
-		Name string
-	}{Name: "world"})
+	_ = templatesupport.Parse(tempDir, "ver", struct {
+		Version string
+	}{Version: "6.1"})
 }
 
 func TestParse_badTemplate(t *testing.T) {
@@ -80,7 +81,7 @@ func TestParse_badTemplate(t *testing.T) {
 			t.Fail()
 		}
 	}()
-	_ = templatesupport.Parse("", "test", struct {
+	_ = templatesupport.Parse("", "version", struct {
 		Bad string
-	}{Bad: "world"})
+	}{Bad: "1.0"})
 }
